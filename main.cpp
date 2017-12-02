3289c46c2f60e9380a98db28eebef6834351ee35
AWieczarek <adamwie12@gmail.com>
2017-12-02 18:34:49 +0100

    Added move function to main

--- main.cpp
+++ main.cpp
@@ -4,116 +4,77 @@
 static bool win          = false;
 static bool player       = false;
 static int roundNumber         = 1;
+static bool attacked;
+static bool moved;
+static std::string input;
+static int positionX, positionY, targetX, targetY;
 
-int main()
-{
-    
-        
-	Board board(10, 10);
+int main() {
+    //openFile(); paweł pisze wczytywanie wszystkoeeeeego z pliku
 
-    do{
-        player=round%2+1;
-        std::cout<<"
+    do {
+        player = roundNumber % 2 + 1;
+        std::cout << "Tura gracza " << player << std::endl;
+        attacked = 0;
+        moved = 0;
+        std::cout << "Jakie pola chcesz sprawdzić        x-nie chce już sprawdzać";
+        do {
+            std::cin >> input;
+            if (input.size() == 2) {
+                positionX = reverse(input[1]);
+                positionY = toint(input[0]);
+            }else if (input.size() == 3) {
+                positionX = reverse(input[1] * 10 + input[2]);
+                positionY = toint(input[0]);
+            }else error("Zbyt duzy input");
 
-    }while(!win)
+            reveal(positionX, positionY);
 
+        } while (!(input.size() == 1 && input[0] == 'x'));
 
+        for (int i = 0; i < 1; i++) {     //to  sie robi raz ale mozna tu wracac :v
+            std::cout << "Czy chcesz sie ruszyc y/n : " << std::endl;
+            std::cin >> input;
+            if (input[0] == 'y') {
+                std::cout << "Wybierz figure                     exit <-wyjdz " << std::endl;
+                std::cin >> input;
+                if (input.size() == 2) {
+                    positionX = reverse(input[1]);
+                    positionY = toint(input[0]);
+                } else if (input.size() == 3) {
+                    positionX = reverse(input[1] * 10 + input[2]);
+                    positionY = toint(input[0]);
+                } else if (input == "exit") {
+                    i--;
+                    continue;
+                } else error("Zły input");
 
+            } else error("Zbyt duzy input");
 
+            std::cout << "Wybierz cel : " << std::endl;
+            std::cin >> input;
+            if (input.size() == 2) {
+                targetX = reverse(input[1]);
+                targetY = toint(input[0]);
+            } else if (input.size() == 3) {
+                targetX = reverse(input[1] * 10 + input[2]);
+                targetY = toint(input[0]);
+            } else if (input == "exit") {
+                i--;
+                continue;
+            } else error("Zły input");
 
-	//openFile();
-	/*trzeba zrobić aby z pliku wczytało nam
-	wymiar planszy
-	współżędne figur
-	współżędne obiektów
-	wszystkie zasoby figur
-	*/
-    do{
-                
-
-        player = round % 2;                                       
-        std::cout << "tura gracza " << player << std::endl;       
-        std::string input;
-        std::cin >> input;
-		int positionX,	 //cyfry
-			positionY,   //literki
-			targetX,
-			targetY;
-		
-		if (input.size() == 2) {
-			positionY = toint(input[0]);
-			positionX = reverse(input[1]);  //prosze napisać reverse now!!!
-		}
-		else if (input.size() == 3) {
-			positionY = toint(input[0]);
-			positionX = reverse(input[1] * 10 + input[2]);
-			
-		}
-		else if (input.size() == 5) {
-			positionY = toint(input[0]);
-			positionX = reverse(input[1]);
-			//targetY = toint
-		}
-		else if (input.size() == 6) {
-			positionY = toint(input[0]);
-			if (input[3] == ' ') {
-				positionY = toint(input[0]);
-				positionX = reverse(input[1]);
-			}
-			else {
-				positionY = toint(input[0]);
-				positionX = reverse(input[1] * 10 + input[2]);
-			}
-		}
-		else if (input.size() == 7) {
-			positionY = toint(input[0]);
-			positionX = reverse(input[1] * 10 + input[2]);
-		}
-		else error("zly input dales!!");
-
-//===========================================================================//
-
-		Figure* address = (Figure*)board[positionX][positionY];
-		Figure* targetAddress = (Figure*)board[targetX][targetY];
-       
-        if (input.size() == 5 || input.size() == 6 || input.size() == 7)
-        {
-			if (targetAddress->Name == "Field")
-			{
-				if (address->canMove(targetX, targetY))
-				{
-					board->swap(positionX, positionY, targetX, targetY);
-					round++;
-				}
-				else error("Nie możesz wykonać takiego ruchu");
-			}
-			else if (targetAddress->Name == "Obstacle") error("walisz w przeszkode");
-            else if(address->isYour(player))  //Sprawdź czy ruszasz swój pionek
-            {
-                GameObject* targetAddress = board[targetX][targetY];
-                else
-                {
-                    //sprawdz czy to twój pionek
-                    if(!address->isYour(player)) //sprawdz jaki pionek tam stoi
-                    {
-						if(address->canAttak(int targetX, int targetY))
-                        {
-                            targetAddress->setHP(-address->dmg); //setHp to znaczy zadaj dmg
-
-                            round++;
-                        }
-                        else error("nie możesz go zaatakować");
-
-                    }
-                    else error("To twój pionek"); // atakujesz swój pionek łosiu
-                }
-
-
+            if(canMove(positionX, positionY, targetX, targetY){
+                move(); // kiedys się zrobi
+                moved=1;
             }
-            else error("To nie twoja figura!");
+            else {
+                std::cout<<"Nie mozesz sie tak ruszyc!" << std::endl;
+                i--;
+                continue;
+            }
         }
-        else error("Zly input!!");
     }while(!win);
     std::cin.get();
 }
-/*=======================================[KONIEC JAK BEDZIE WIECEJ NIZ 100 TO SIE ZABIJE]========================================*/
+
