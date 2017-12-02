9e4ce7e3c0f9e080cc0fe00cd56a0df6994fcb22
AWieczarek <adamwie12@gmail.com>
2017-12-02 16:47:57 +0100

    Main od nowa

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "Header.h"
 #include "Board.h"
 
-static bool win          = 0;
-static bool player       = 0;
+static int win          = 0;
+static int player       = 0;
 static int round         = 1;
 
 int main()
@@ -11,25 +11,35 @@ int main()
         
 	Board board(10, 10);
 
-    //openFile();
+    do{
+        player=round%2+1;
+        std::cout<<"
+
+    }while(!win)
+
+
+
+
+
 	/*trzeba zrobić aby z pliku wczytało nam
 	wymiar planszy
 	współżędne figur
 	współżędne obiektów
 	wszystkie zasoby figur
 	*/
+    /*
     do{
-                
 
-        player = round % 2;                                       
-        std::cout << "tura gracza " << player << std::endl;       
+
+        player = round % 2;
+        std::cout << "tura gracza " << player << std::endl;
         std::string input;
         std::cin >> input;
 		int positionX,	 //cyfry
 			positionY,   //literki
 			targetX,
 			targetY;
-		
+
 		if (input.size() == 2) {
 			positionY = toint(input[0]);
 			positionX = reverse(input[1]);  //prosze napisać reverse now!!!
@@ -37,7 +47,7 @@ int main()
 		else if (input.size() == 3) {
 			positionY = toint(input[0]);
 			positionX = reverse(input[1] * 10 + input[2]);
-			
+
 		}
 		else if (input.size() == 5) {
 			positionY = toint(input[0]);
@@ -65,7 +75,7 @@ int main()
 
 		Figure* address = (Figure*)board[positionX][positionY];
 		Figure* targetAddress = (Figure*)board[targetX][targetY];
-       
+
         if (input.size() == 5 || input.size() == 6 || input.size() == 7)
         {
 			if (targetAddress->Name == "Field")
@@ -77,10 +87,10 @@ int main()
 				}
 				else error("Nie możesz wykonać takiego ruchu");
 			}
-			else if (targetAddress->Name == "Obstacle") error("walisz w przeszkode");           
+			else if (targetAddress->Name == "Obstacle") error("walisz w przeszkode");
             else if(address->isYour(player))  //Sprawdź czy ruszasz swój pionek
-            {               
-                GameObject* targetAddress = board[targetX][targetY];               
+            {
+                GameObject* targetAddress = board[targetX][targetY];
                 else
                 {
                     //sprawdz czy to twój pionek
@@ -89,21 +99,21 @@ int main()
 						if(address->canAttak(int targetX, int targetY))
                         {
                             targetAddress->setHP(-address->dmg); //setHp to znaczy zadaj dmg
-                            
+
                             round++;
                         }
                         else error("nie możesz go zaatakować");
-                        
+
                     }
                     else error("To twój pionek"); // atakujesz swój pionek łosiu
                 }
-                
-                
+
+
             }
             else error("To nie twoja figura!");
         }
         else error("Zly input!!");
-    }while(!win);
+    }while(!win);*/
     std::cin.get();
 }
-/*=======================================[KONIEC JAK BEDZIE WIECEJ NIZ 100 TO SIE ZABIJE]========================================*/
+
