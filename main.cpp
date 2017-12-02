5d0abb4ba9b125b59319ad7f16e754951e96dab1
AWieczarek <adamwie12@gmail.com>
2017-12-02 15:22:53 +0100

    Debugowanie, zmiany w nagłówkach i w ogóle w klasach, nie wiem bo mnie posadzili tylko do commitowania
    Signed-off-by: Michał Krzysztof Feiler <archiet@platinum.edu.pl>

--- main.cpp
+++ main.cpp
@@ -3,33 +3,29 @@
 
 static bool win          = 0;
 static bool player       = 0;
-static int round         = 1;
+static int roundNumber         = 1;
 
 int main()
 {
-    
-        
-	Board board(10, 10);
-
-    //openFile();
+	//openFile();
 	/*trzeba zrobić aby z pliku wczytało nam
 	wymiar planszy
 	współżędne figur
 	współżędne obiektów
 	wszystkie zasoby figur
 	*/
-    do{
-                
+	Board board(10, 10); //zmienić na zczytanie z pliku rozmiaru z planszy
 
-        player = round % 2;                                       
-        std::cout << "tura gracza " << player << std::endl;       
-        std::string input;
-        std::cin >> input;
-		int positionX,	 //cyfry
-			positionY,   //literki
-			targetX,
-			targetY;
-		
+	do {
+		player = roundNumber % 2;
+		std::cout << "tura gracza " << player << std::endl;
+		std::string input;
+		std::cin >> input;
+		int positionX;	 //cyfry
+		int	positionY;   //literki
+		int	targetX;	//cyfry
+		int	targetY;	//literki
+
 		if (input.size() == 2) {
 			positionY = toint(input[0]);
 			positionX = reverse(input[1]);  //prosze napisać reverse now!!!
@@ -37,7 +33,7 @@ int main()
 		else if (input.size() == 3) {
 			positionY = toint(input[0]);
 			positionX = reverse(input[1] * 10 + input[2]);
-			
+
 		}
 		else if (input.size() == 5) {
 			positionY = toint(input[0]);
@@ -59,10 +55,13 @@ int main()
 			positionY = toint(input[0]);
 			positionX = reverse(input[1] * 10 + input[2]);
 		}
-		else error("zly input dales!!");
+		else error("Za duży input!");
 
+	} while (!win);
+	std::cin.get();
+}
 //===========================================================================//
-
+/*
 		Figure* address = (Figure*)board[positionX][positionY];
 		Figure* targetAddress = (Figure*)board[targetX][targetY];
        
@@ -72,8 +71,8 @@ int main()
 			{
 				if (address->canMove(targetX, targetY))
 				{
-					board->swap(positionX, positionY, targetX, targetY);
-					round++;
+					board.swap(positionX, positionY, targetX, targetY);
+					roundNumber++;
 				}
 				else error("Nie możesz wykonać takiego ruchu");
 			}
@@ -81,24 +80,17 @@ int main()
             else if(address->isYour(player))  //Sprawdź czy ruszasz swój pionek
             {               
                 GameObject* targetAddress = board[targetX][targetY];               
-                else
-                {
                     //sprawdz czy to twój pionek
                     if(!address->isYour(player)) //sprawdz jaki pionek tam stoi
                     {
-						if(address->canAttak(int targetX, int targetY))
-                        {
-                            targetAddress->setHP(-address->dmg); //setHp to znaczy zadaj dmg
-                            
-                            round++;
-                        }
-                        else error("nie możesz go zaatakować");
-                        
+						if (address->canAttak(int targetX, int targetY))
+						{
+							targetAddress->setHP(-address->dmg); //setHp to znaczy zadaj dmg
+							round++;
+						}
+                        else error("nie możesz go zaatakować");                       
                     }
-                    else error("To twój pionek"); // atakujesz swój pionek łosiu
-                }
-                
-                
+                    else error("To twój pionek"); // atakujesz swój pionek łosiu               
             }
             else error("To nie twoja figura!");
         }
