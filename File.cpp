7d6ceb1aa18e49b3e043d9b1d68bb722ba5fc3d1
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2017-12-03 08:47:12 +0100

    Made GetFromFile operate on a Board pointer and simplified it a bit. Added File.cpp to CMake.

--- File.cpp
+++ File.cpp
@@ -1,13 +1,14 @@
 #include "Header.h"
+#include "Board.h"
 
-void GetFromFile()
+void GetFromFile(Board &board)
 {
     std::fstream file;
     file.open("Save.txt",std::ios::in);
 
-    if(file.good() == false)
+    if(!file.good())
     {
-        error("File is not Exist!!!")
+        error("File is not Exist!!!");
         return;
     }
 
@@ -47,7 +48,7 @@ void GetFromFile()
 
         int Position_File_X = 0;
         int Position_File_Y = 0;
-        std::string Name_File = "KUPA";
+        std::string Name_File;
         bool Own_File;
         int HP_File = 0;
         int Dmg_File = 0;
@@ -80,9 +81,9 @@ void GetFromFile()
             i++;
         }
 
-        if(Name_File == "Obst")
+        if(Name_File == obstacleNameString)
         {
-            board[Position_File_X][Position_File_Y] -> "Obstacle";
+            board[Position_File_X][Position_File_Y] = new Obstacle;
             continue;
         }       //tu jest tworzone, jesli mamy przeszkode
 
@@ -112,30 +113,24 @@ void GetFromFile()
 
         //mam Dmg_File
 
-        if(Name_File == "Pawn")
-        {
-            board[Position_File_X][Position_File_Y] = new Pawn(HP_File,Dmg_File,"COS",Own_File,Position_File_X,Position_File_Y);
-        }
-        else if(Name_File == "Hors")
-        {
-            board[Position_File_X][Position_File_Y] = new Horseman(HP_File,Dmg_File,"COS",Own_File,Position_File_X,Position_File_Y);
-        }
-        else if(Name_File == "Bish")
-        {
-            board[Position_File_X][Position_File_Y] = new Bishop(HP_File,Dmg_File,"COS",Own_File,Position_File_X,Position_File_Y);
-        }
-        else if(Name_File == "Towe")
-        {
-            board[Position_File_X][Position_File_Y] = new Tower(HP_File,Dmg_File,"COS",Own_File,Position_File_X,Position_File_Y);
-        }
-        else if(Name_File == "Quee")
-        {
-            board[Position_File_X][Position_File_Y] = new Queen(HP_File,Dmg_File,"COS",Own_File,Position_File_X,Position_File_Y);
-        }
-        else if(Name_File == "King")
-        {
-            board[Position_File_X][Position_File_Y] = new King(HP_File,Dmg_File,"COS",Own_File,Position_File_X,Position_File_Y);
-        }
+        if (Name_File == figNames[PAWN])
+            board[Position_File_X][Position_File_Y] = //new Pawn(HP_File,Dmg_File,"COS",Own_File,Position_File_X,Position_File_Y);
+                    new Pawn(Own_File, HP_File, Dmg_File);
+        else if (Name_File == figNames[KNIGHT])
+            board[Position_File_X][Position_File_Y] = //new Horseman(HP_File,Dmg_File,"COS",Own_File,Position_File_X,Position_File_Y);
+                    new Knight(Own_File, HP_File, Dmg_File);
+        else if (Name_File == figNames[BISHOP])
+            board[Position_File_X][Position_File_Y] = //new Bishop(HP_File,Dmg_File,"COS",Own_File,Position_File_X,Position_File_Y);
+                    new Bishop(Own_File, HP_File, Dmg_File);
+        else if (Name_File == figNames[ROOK])
+            board[Position_File_X][Position_File_Y] = //new Tower(HP_File,Dmg_File,"COS",Own_File,Position_File_X,Position_File_Y);
+                    new Rook(Own_File, HP_File, Dmg_File);
+        else if (Name_File == figNames[QUEEN])
+            board[Position_File_X][Position_File_Y] = //new Queen(HP_File,Dmg_File,"COS",Own_File,Position_File_X,Position_File_Y);
+                    new Queen(Own_File, HP_File, Dmg_File);
+        else if (Name_File == figNames[KING])
+            board[Position_File_X][Position_File_Y] = //new King(HP_File,Dmg_File,"COS",Own_File,Position_File_X,Position_File_Y);
+                    new King(Own_File, HP_File, Dmg_File);
 
         //tutaj tworzymy figure...
 
