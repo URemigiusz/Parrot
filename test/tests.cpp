657c64ea91f06e10dd7e4a65a4fcc227e702fede
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2017-12-03 09:41:23 +0100

    testowanie figur, bo testowanie boarda jeszcze nie działa bo się wywala na dekonstruktorach

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,3 +6,8 @@ TEST_CASE("testujemy abs", "[abs]") {
     REQUIRE(abs(-1)==1);
     //REQUIRE(abs(-2)==1);
 }
+
+TEST_CASE("testujemy figury", "[fig]") {
+    Figure* pawn = new Pawn(WHITE);
+    REQUIRE(pawn->figureDMG>0);
+}
