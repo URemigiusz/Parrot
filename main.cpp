a55b6bec0ae04ee7bca2e11adafefd880b317a19
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2018-02-17 16:15:53 +0100

    made main compile

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ static int positionX, positionY, targetX, targetY;
 void move() {}
 
 int main() {
-    Board* board = GetFromFile();
+    Board board = GetFromFile();
 
     //setlocale( LC_ALL, "pl_PL" );   nie weim jak, ale zostawiam żeb wiedzieć że chcemy polskie znaki
 
@@ -27,14 +27,14 @@ int main() {
         do {
             std::cin >> input;
             if (input.size() == 2) {
-                positionX = reverse(input[1], board->getY());
-                positionY = toInt(input[0], board->getX());
+                positionX = reverse(input[1], board.getY());
+                positionY = toInt(input[0], board.getX());
             }else if (input.size() == 3) {
-                positionX = reverse(input[1] * 10 + input[2], board->getY());
-                positionY = toInt(input[0], board->getX());
+                positionX = reverse(input[1] * 10 + input[2], board.getY());
+                positionY = toInt(input[0], board.getX());
             }else error("Zbyt duzy input");
 
-            (*board)[positionX][positionY]->reveal();
+            board[positionX][positionY]->reveal();
 
         } while (!(input.size() == 1 && input[0] == 'x'));
 //======================================MOVE============================================//
@@ -45,11 +45,11 @@ int main() {
                 std::cout << "Wybierz figure                     exit <-wyjdz " << std::endl;
                 std::cin >> input;
                 if (input.size() == 2) {
-                    positionX = reverse(input[1], board->getY());
-                    positionY = toInt(input[0], board->getX());
+                    positionX = reverse(input[1], board.getY());
+                    positionY = toInt(input[0], board.getX());
                 } else if (input.size() == 3) {
-                    positionX = reverse(input[1] * 10 + input[2], board->getY());
-                    positionY = toInt(input[0], board->getX());
+                    positionX = reverse(input[1] * 10 + input[2], board.getY());
+                    positionY = toInt(input[0], board.getX());
                 } else if (input == "exit") {
                     i--;
                     continue;
@@ -63,17 +63,17 @@ int main() {
             std::cout << "Wybierz cel :                          exit-wyjscie" << std::endl;
             std::cin >> input;
             if (input.size() == 2) {
-                targetX = reverse(input[1], board->getY());
-                targetY = toInt(input[0], board->getX());
+                targetX = reverse(input[1], board.getY());
+                targetY = toInt(input[0], board.getX());
             } else if (input.size() == 3) {
-                targetX = reverse(input[1] * 10 + input[2], board->getY());
-                targetY = toInt(input[0], board->getX());
+                targetX = reverse(input[1] * 10 + input[2], board.getY());
+                targetY = toInt(input[0], board.getX());
             } else if (input == "exit") {
                 i--;
                 continue;
             } else error("Zły input");
 
-            if(board->canMove(positionX, positionY, targetX, targetY)){         //drogi remku z przyszłości wiemy że to inaczej
+            if(board.canMove(positionX, positionY, targetX, targetY)){         //drogi remku z przyszłości wiemy że to inaczej
                 move(); // kiedys się zrobi
                 moved= true;
             }
@@ -92,11 +92,11 @@ int main() {
                 std::cout << "Wybierz figure :                    exit <-wyjdz " << std::endl;
                 std::cin >> input;
                 if (input.size() == 2) {
-                    positionX = reverse(input[1], board->getY());
-                    positionY = toInt(input[0], board->getX());
+                    positionX = reverse(input[1], board.getY());
+                    positionY = toInt(input[0], board.getX());
                 } else if (input.size() == 3) {
-                    positionX = reverse(input[1] * 10 + input[2], board->getY());
-                    positionY = toInt(input[0], board->getX());
+                    positionX = reverse(input[1] * 10 + input[2], board.getY());
+                    positionY = toInt(input[0], board.getX());
                 } else if (input == "exit") {
                     i--;
                     continue;
@@ -109,19 +109,19 @@ int main() {
             std::cout << "Wybierz cel : " << std::endl;
             std::cin >> input;
             if (input.size() == 2) {
-                targetX = reverse(input[1], board->getY());
-                targetY = toInt(input[0], board->getX());
+                targetX = reverse(input[1], board.getY());
+                targetY = toInt(input[0], board.getX());
             } else if (input.size() == 3) {
-                targetX = reverse(input[1] * 10 + input[2], board->getY());
-                targetY = toInt(input[0], board->getX());
+                targetX = reverse(input[1] * 10 + input[2], board.getY());
+                targetY = toInt(input[0], board.getX());
             } else if (input == "exit") {
                 i--;
                 continue;
             }
             else error("Zły input");
-            if(board->canAttack(positionX, positionY, targetX, targetY)){       //drogi remku z przyszłości wiemy że to inaczej
-                auto * fig = (Figure *) (*board)[positionX][positionY];
-                auto * tar = (Figure *) (*board)[targetX][targetY];
+            if(board.canAttack(positionX, positionY, targetX, targetY)){       //drogi remku z przyszłości wiemy że to inaczej
+                auto * fig = (Figure *) board[positionX][positionY];
+                auto * tar = (Figure *) board[targetX][targetY];
                 if (setHP(fig->figureDMG, tar, player)) win = true;             //zabicie króla kończy grę
                 else attacked = true;
             }
